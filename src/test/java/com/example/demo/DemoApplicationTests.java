a0cbfb690498f4e98a70218765d7f34093875455
Licious <Licious@Admins-MacBook-Pro.local>
2021-11-27 23:36:20 +0530

    testing

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoApplicationTests {
 
 	@Test
+	@DisplayName("Test1")
 	void contextLoads() {
 	}
 
